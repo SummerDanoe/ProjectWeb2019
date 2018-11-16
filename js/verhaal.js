96ac1b91023983b07da7ccf2cde80212bbdf945e
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2018-11-16 21:48:45 +0100

    verhaal.html klaar

---
+++ js/verhaal.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+var button = document.querySelector('button:nth-of-type(3)');
+var buttonImage = document.querySelector('button:nth-of-type(3) img').src;
+
+/*function toggleImage() {
+	
+	if (buttonImage.indexOf('opslaan.png')!=-1) {
+		document.querySelector('button:nth-of-type(3) img').src  = 'img/opgeslagen.png';
+        }
+	else {
+		document.querySelector('button:nth-of-type(3) img').src = 'img/opslaan.png';
+       }
+
+}*/
+function toggleImage() {
+	
+	if (buttonImage.indexOf('opslaan.png')!=-1) {
+		document.querySelector('button:nth-of-type(3) img').src  = 'img/opslaanwit.gif';
+		setTimeout(200);
+		document.querySelector('button:nth-of-type(3) img').src  = 'img/opgeslagen.png';
+        }
+	else {
+		document.querySelector('button:nth-of-type(3) img').src = 'img/opslaan.png';
+       }
+
+}
+button.addEventListener("click", toggleImage);
