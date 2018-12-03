da76199543ed51e215d6ce4e185f59bdea36a038
SummerDanoe <31731680+SummerDanoe@users.noreply.github.com>
2018-12-03 19:27:24 +0100

    test microinteractie met bug
    microinteractie werkt pas bij de tweede keer klikken.

--- js/test.js
+++ js/test.js
@@ -1,17 +1,48 @@
-var opslaan = "img/opslaan.png";
+img.src = gif;img.src = gif;img.src = gif;img.src = gif;img.src = gif;img.src = gif;var opslaan = "img/opslaan.png";
 var opgeslagen = "img/opgeslagen.png";
+var gif = "img/opslaanwit.gif";
 var state = false;
-var opslaanImg = document.getElementById("img");
+var img = document.getElementById("img");
 
-	opslaanImg.addEventListener("click", imgToggle)
+	img.addEventListener("click", test);
+	//img.addEventListener("click", imgToggle);
 
-function imgToggle(){
+/*function microInteractie(){
+	animatie();
+	//setTimeout(reset, 500);
+	setTimeout(imgToggle, 500);
+}*/
+
+/*function animatie(){
+	img.src = gif;
+}*/
+function opgeslagenTimeout(){
+	img.src = opgeslagen;
+}
+/*function reset(){
+	img.src = opgeslagen;
+}*/
+//Toggle image bron: https://stackoverflow.com/questions/39892794/toggle-image-in-pure-javascript
+/*function imgToggle(){
 	if(state){
+	state = false;
   	img.src = opgeslagen;
-    state = false;
+    
   }
   else{
-  	img.src = opslaan;
     state = true;
+	img.src = opslaan;
+  }
+}*/
+
+function test(){
+	if(state){
+	state = false;
+	img.src = gif;
+	setTimeout(opgeslagenTimeout,500);
+	}
+  else{
+    state = true;
+	img.src = opslaan;
   }
 }
